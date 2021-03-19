cffbc7a80c1ff37480edcde322063f952d18cdf5
Crystal <ceceherrera490@gmail.com>
2021-03-19 16:05:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.querySelector("#input");
+// const div = document.querySelectorAll("div")[5];
+
 function addingEventListener() {
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
 }
+
+input.addEventListener("click", addingEventListener);
+// div.addEventListener("click", addingEventListener);
