69bdf212c62c091781fa83229f4d1104d27e9ba1
Crystal <ceceherrera490@gmail.com>
2021-04-27 21:56:32 -0400

    commit tree 519c40f7bef52627342ba474a55348e1230a1d44

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const input = document.querySelector("#input");
-// const div = document.querySelectorAll("div")[5];
+// const input = document.querySelector("#input");
+// // const div = document.querySelectorAll("div")[5];
+
+// function addingEventListener() {
+//   input.addEventListener("click", function () {
+//     alert("I was clicked!");
+//   });
+// }
+
+// input.addEventListener("click", addingEventListener);
+// // div.addEventListener("click", addingEventListener);
+
+// const input = document.querySelector("#input");
+// input.addEventListener("click", () => {
+//   alert("clicked");
+// });
+
+const input = document.getElementById("input");
+const div = document.querySelectorAll("div")[5];
+
+// input.addEventListener("click", clickAlert);
+// div.addEventListener("click", clickAlert);
 
 function addingEventListener() {
-  input.addEventListener("click", function () {
+  input.addEventListener("click", () => {
     alert("I was clicked!");
   });
 }
-
 input.addEventListener("click", addingEventListener);
-// div.addEventListener("click", addingEventListener);
+div.addEventListener("click", addingEventListener);
